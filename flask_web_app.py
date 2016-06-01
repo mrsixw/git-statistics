81c978ff39afe9bbf3b945204c842440b63799b7
Steve Williams <mrsixw@gmail.com>
2016-06-01 21:42:23 +0100

    Add first bits of web app

---
+++ flask_web_app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
+import os
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/branch/<branch>')
+def branch_index(branch):
+
+    commits = os.listdir('data/'+branch)
+    print commits
+
+    for commit in commits:
+        full_file_path = 'data/%s/%s' % (branch, commit)
+        with open(full_file_path,'r') as f:
+            lines = f.readlines()
+            #print lines
+
+
+    print "Total commits %d" % (len(commits))
+
+    return render_template('branch.html', branch = branch)
+
+@app.route('/')
+def index():
+
+    # list the branches
+    branches = os.listdir('data')
+    print branches
+
+    return render_template('index.html', branches = branches)
+
+if __name__ == '__main__':
+    app.run(debug=True)
